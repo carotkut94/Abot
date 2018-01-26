50ab19a560faed0508b7a98becb8dbb6ebb3dc7f
DeathCode <organisetohack@gmail.com>
2018-01-26 15:10:13 +0530

    Added intial Part 1 of the video series

---
+++ app/src/main/java/com/example/deathcode/abot/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.deathcode.abot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText userInput;
+    RecyclerView recyclerView;
+    MessageAdapter messageAdapter;
+    List<ResponseMessage> responseMessageList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userInput = findViewById(R.id.userInput);
+        recyclerView = findViewById(R.id.conversation);
+        responseMessageList = new ArrayList<>();
+        messageAdapter = new MessageAdapter(responseMessageList, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+        recyclerView.setAdapter(messageAdapter);
+
+        userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEND) {
+                    ResponseMessage responseMessage = new ResponseMessage(userInput.getText().toString(), true);
+                    responseMessageList.add(responseMessage);
+                    ResponseMessage responseMessage2 = new ResponseMessage(userInput.getText().toString(), false);
+                    responseMessageList.add(responseMessage2);
+                    messageAdapter.notifyDataSetChanged();
+                    if (!isLastVisible())
+                        recyclerView.smoothScrollToPosition(messageAdapter.getItemCount() - 1);
+                }
+                return false;
+            }
+        });
+    }
+    boolean isLastVisible() {
+        LinearLayoutManager layoutManager = ((LinearLayoutManager) recyclerView.getLayoutManager());
+        int pos = layoutManager.findLastCompletelyVisibleItemPosition();
+        int numItems = recyclerView.getAdapter().getItemCount();
+        return (pos >= numItems);
+    }
+}
